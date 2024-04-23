e3c2a82db0ef3225c289f6a224d672b14c941c6b
peterh3g <peterh3g@gmail.com>
2024-04-23 21:25:48 +0200

    gitpush: code cleanup

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,36 +1,3 @@
 export default defineAppConfig({
-  app: {
-    name: 'PeterH3G',
-    description: 'Web Developer',
-    repository: 'nuxt-shadcn-bun',
-  },
 
-  layout: {
-    // Default layout configuration
-    default: {
-      isCentered: true,
-      maxWidth: '800px', // if isCentered false
-    },
-
-    // Global component configurations
-    global: {
-      button: {
-        iconOnly: true,
-      },
-    },
-
-    // Individual component configurations
-    buttonDeveloper: {
-      show: true,
-      iconOnly: true,
-    },
-    buttonDrawer: {
-      show: true,
-      iconOnly: false,
-    },
-    buttonTheme: {
-      show: true,
-      iconOnly: true,
-    },
-  },
 })
